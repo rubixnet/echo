272d74353d35121c061587fcb1f7386961036b82
rubix <gauravbhaturkar18@gmail.com>
2026-05-30 23:56:43 +0530

    package update for update

---
+++ src/app/api/auth/google/route.ts
@@ -0,0 +1,22 @@
+import {WorkOS} from "@workos-inc/node";
+import {NextResponse} from "next/server";
+
+const workos = new WorkOS(process.env.WORKOS_API_KEY!);
+
+export async function GET(req: Request) {
+    const requestUrl = new URL(req.url);
+    const clientId = process.env.WORKOS_CLIENT_ID!;
+    const redirectUri = new URL("/api/auth/callback", requestUrl.origin).toString();
+
+    if (!clientId || !redirectUri) {
+        return NextResponse.json({error: "Missing client id or redirect uri"}, {status: 500});
+    } 
+
+    const authorizationUrl = workos.userManagement.getAuthorizationUrl({
+        clientId,
+        provider: "GoogleOAuth", 
+        redirectUri,
+    })
+
+    return NextResponse.json(authorizationUrl);
+}
