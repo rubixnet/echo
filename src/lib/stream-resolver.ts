cba2d924261de8aa16859f5332238112562cf2a8
rubix <gauravbhaturkar18@gmail.com>
2026-06-18 12:30:52 +0530

    rest of audioprovider stuff

--- src/lib/stream-resolver.ts
+++ src/lib/stream-resolver.ts
@@ -2,6 +2,5 @@ export async function getClientSideStreamUrl(videoId: string) {
 
     const res = await fetch(`https://api.piped.projectsegfau.lt/streams/${videoId}`);
     const data = await res.json();
-
     return data.audioStreams.find((s: any) => s.audioTrackId)?.url || data.audioStreams[0].url;
 }
