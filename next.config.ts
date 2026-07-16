9fe27c7e11b44bf8a0e2c232d5c09a1633e21e74
rubix <gauravbhaturkar18@gmail.com>
2026-07-16 23:15:15 +0530

    next.config update for update, app now has incomplete liquid ui componenet from figma file ta

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback = {
+        ...(config.resolve.fallback || {}),
         fs: false,
         net: false,
         tls: false,
