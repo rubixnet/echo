57f00555ef41d4a39059d8e05cd06ad84bbb3295
rubix <gauravbhaturkar18@gmail.com>
2026-07-27 21:48:44 +0530

    more readme changes - part of: Revert "m,id work"

--- convex/schema.ts
+++ convex/schema.ts
@@ -68,7 +68,6 @@ export default defineSchema({
 
   playlists: defineTable({
     name: v.string(),
-    isPineed: v.boolean(),
     userId: v.id('users'),
     createdAt: v.number(),
   }).index("by_user", ["userId"]),
